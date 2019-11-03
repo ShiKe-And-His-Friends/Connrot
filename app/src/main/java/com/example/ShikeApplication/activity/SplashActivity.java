a2455bb4146bf2123aac032960b48e2f367d5f15
sk95120 <765871277@qq.com>
2019-11-03 22:08:08 +0800

    commit issue： 1、Spalsh. 2、

---
+++ app/src/main/java/com/example/ShikeApplication/activity/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.example.ShikeApplication.activity;
+
+import android.os.Bundle;
+
+import com.example.ShikeApplication.R;
+
+public class SplashActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
---
+++ app/src/main/java/com/example/ShikeApplication/view/MeasureScreenSizeLayout.java
@@ -0,0 +1,23 @@
+package com.example.ShikeApplication.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class MeasureScreenSizeLayout extends RelativeLayout {
+    public MeasureScreenSizeLayout(Context context) {
+        super(context);
+    }
+
+    public MeasureScreenSizeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MeasureScreenSizeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MeasureScreenSizeLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
