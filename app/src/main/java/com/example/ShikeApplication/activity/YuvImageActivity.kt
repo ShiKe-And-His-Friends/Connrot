dac024adb0cf713d800dee068085379d998682a4
sk95120 <765871277@qq.com>
2020-10-12 17:13:26 +0800

    commit： 1. Fullscreen and landspace into yuv image activity;

--- app/src/main/java/com/example/ShikeApplication/activity/YuvImageActivity.kt
+++ app/src/main/java/com/example/ShikeApplication/activity/YuvImageActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ShikeApplication.activity
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -13,22 +14,34 @@ import java.io.IOException
 
 class YuvImageActivity : BaseActivity() {
 
+    var TAG = "YuvImageActivity"
     var width = 1280
     var height = 720
-    private var mglsuface: GLSurface? = null
     private var mrender: MyGLRender? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_yuv_image)
-        mglsuface = findViewById(R.id.preview)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+        Log.i(TAG ,"yuv image activity on create")
+
+        var mglsuface: GLSurface = findViewById(R.id.preview)
+        Log.i(TAG ,"gl surface not null is " + (mglsuface!=null))
         mrender = MyGLRender(mglsuface)
+        mglsuface.setRenderer(mrender)
         mrender!!.update(width, height)
         var file: File = File("/sdcard/test.yuv")
 
         if (!file.exists()) {
             // A test.yuv file that format is YUV NY14 4:2:0 size is height 720 width 1280
             Toast.makeText(this, "/sdcard/test.yuv 1280X720 ，文件不存在", Toast.LENGTH_LONG).show()
+            object : Thread() {
+                override fun run() {
+                    super.run()
+                    sleep(3 * 1000)
+                    finish()
+                }
+            }.start()
         } else {
             object : Thread() {
                 override fun run() {
@@ -54,7 +67,7 @@ class YuvImageActivity : BaseActivity() {
                             break
                         }
                         mrender!!.update(input)
-                        Log.i("thread", "thread is executing hasRead: $hasRead")
+                        Log.i(TAG, "thread is executing hasRead: $hasRead")
                         try {
                             sleep(100)
                         } catch (e: InterruptedException) {
@@ -64,6 +77,22 @@ class YuvImageActivity : BaseActivity() {
                 }
             }.start()
         }
+        Log.i(TAG ,"yuv image activity on create success")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG ,"yuv image activity on resume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG ,"yuv image activity on restart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG ,"yuv image activity on destory")
     }
 
 }
